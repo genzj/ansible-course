470e46a83903a1a4625f24254db2a057cba126f2
ZHU Jie <zj0512@gmail.com>
2019-02-14 13:17:28 +0800

    add random password filter example

---
+++ e102-custom-filter/plugins/filter/random_password.py
@@ -0,0 +1,21 @@
+from random import choices
+from string import ascii_letters, digits
+
+
+DEFAULT_CANDIDATES = ascii_letters + digits
+
+
+def random_str(length, candidates=DEFAULT_CANDIDATES):
+    return ''.join(choices(candidates, k=length))
+
+
+class FilterModule(object):
+    ''' Query filter '''
+
+    # test with
+    #   ansible -m debug -a 'msg="{{ 8 | random_password}}"' server
+
+    def filters(self):
+        return {
+            'random_password': random_str,
+        }
